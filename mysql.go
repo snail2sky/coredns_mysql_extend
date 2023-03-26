913c72bac1bb73981aa7e8ec8100410157187c53
snail <49757958+snail2sky@users.noreply.github.com>
2023-03-26 13:12:32 +0000

    Dev: 8 debug log

--- mysql.go
+++ mysql.go
@@ -59,7 +59,7 @@ func (m *Mysql) Name() string {
 }
 
 func (m *Mysql) ParseConfig(c *caddy.Controller) error {
-	log.Debugf("hello world, debug")
+	log.Info("hello world, debug")
 	// for c.Next() {
 	// 	log.Debugf("%#v", c)
 	// 	if !c.Args(&m.DomainsTable, &m.RecordsTable) {
